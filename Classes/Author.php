05e8906ed76af18cf4cbb188dfd2a791040d44ce
Zachary Sanchez <Zacharyesanchez22@gmail.com>
2019-10-16 21:39:08 -0600

    added author.php

--- Classes/Author.php
+++ Classes/Author.php
@@ -2,6 +2,10 @@
 
 namespace Zachary22-Sanchez/ObjectOrientedProject;
 
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+
+use Ramsey\Uuid\Uuid;
+
 class Author  {
 	use ValidateUuid;
 	/**
@@ -11,32 +15,107 @@ class Author  {
 	private $authorId;
 	/**
 	 * at handle for this Profile; this is a unique index
-	 * @var string $profileAtHandle
+	 * @var string $authorAtHandle
 	 **/
-	private $authorAvatarUrl;
+	public function getAuthorId(): Uuid {
+		return ($this->authorId);
+	}
+/**
+ * mutator method for author id
+ *
+ * @param Uuid| string $newAuthorId value of new author id
+ * @throws \RangeException if $newAuthorId is not positive
+ * @throws \TypeError if the profile Id is not
+ */
+public function setAuthorId( $newAuthorId): void {
+	try {
+		$uuid = self::validateUuid($newAuthorId);
+	} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+		$exceptionType = get_class($exception);
+		throw(new $exceptionType($exception->getMessage(), 0, $exception));
+	}
+	//convert and store the author id
+	$this->authorId = $uuid;
+}
+/**
+ * accessor method for account activation token
+ *
+ * @return string value of the activation token
+ */
+public function getProfileActivationToken() : ?string {
+	return ($this->getAuthorActivationToken);
+}
+/**
+ *mutator method for account activation token
+ *
+ *@param string $newAuthorActivationToken
+ *@throws \InvalidArgumentException if the token is not a string or insecure
+ *@throws \RangeException if the token is not exactly 32 characters
+ *@throws \TypeError if the activation token is not a string
+ */
+public function setAuthorActivationToken(?string $newAuthorActivationToken): void {
+	if($newAuthorActivationToken === null) {
+		$this->setAuthorActivationToken = null;
+		return;
+	}
+	$newAuthorActivationToken = strtolower(trim($newAuthorActivationToken));
+	if(ctype_xdigit($newAuthorActivationToken) === false) {
+		throw(new\RangeException("user activation is not valid"));
+	}
+	//make sure user activation token is only 32 characters
+	if(strlen($newAuthorActivationToken) !== 32) {
+		throw(new\RangeException("user activation token has to be 32"));
+	}
+	$this->authorActivationToken = $newAuthorActivationToken;
+}
+/**
+ * accessor method for at handle
+ *
+ * @return string value of at handle
+ */
+public function authorAvatarUrl(): string {
+	return ($this->authorAvatarUrl);
+}
+/**
+ *mutator method for at handle
+ *
+ *@param string $newAuthorAvatarUrl new value of at handle
+ *@throws \InvalidArgumentException if $newAtHandle is not a string or insecure
+ *@throws \RangeException if $newAtHandle is > 32 characters
+ *@throws \TypeError if $newAtHandle is not a string
+ */
+public function setAuthorAvatarUrl(string $newAuthorAvatarUrl) : void {
+	//verify the at handle is secure
+	$newAuthorAvatarUrl = trim($newAuthorAvatarUrl);
+	$newAuthorAvatarUrl = filter_var($newAuthorAvatarUrl, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	if(empty($authorAvatarUrl) === true) {
+		throw(new \InvalidArgumentException("profile at handle is empty or insecure"));
+	}
+	// store the at handle
+	$this->authorAvatarUrl = $newAuthorAvatarUrl;
+}
+/**
+ * mutator method for email
+ *
+ * @param string $newAuthorEmail new value of email
+ * @throws \InvalidArgumentException if $newEmail is not valid email or insecure
+ * @throws \RangeException if $newEmail is > 128 characters
+ * @throws \TypeError if $newEmail is not a string
+ */
+public function setAuthorEmail(string $newAuthorEmail): void {
+	//verify the email is secure
+	$newAuthorEmail = trim($newAuthorEmail);
+	$newAuthorEmail = filter_var($newAuthorEmail, FILTER_VALIDATE_EMAIL);
+	if (empty($newAuthorEmail) === true) {
+		throw(new \InvalidArgumentException("profile email is empty or insecure"));
+	}
+	//verify the email will fit in the database
+	if(strlen($newAuthorEmail) > 128) {}
+	throw(new \RangeException("profile email is too large"));
+}
+//store the email
+$this->authorEmail = $newAuthorEmail;
+	}
 	/**
-	 * token handed out to verify that the profile is valid and not malicious.
-	 *v@var $profileActivationToken
-	 **/
-	private $authorActivationToken;
-	/**
-	 * token handed out to verify that the profile is valid and not malicious.
-	 *v@var $authorActivationToken
-	 **/
-	private $authorEmail;
-	/**
-	 * email for this Profile; this is a unique index
-	 * @var string $authorEmail
-	 **/
-	private $authorHash;
-	/**
-	 * hash for this password
-	 * @var $authorHash
-	 **/
-	private $authorUsername;
-	/**
-	 * salt for profile password
-	 *
-	 * @var $profileSalt
+	 * access method for
 	 */
-	private $profileSalt;
